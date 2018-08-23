e01c06d60c00c56684946d52c74161c3785449b9
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-23 15:57:06 -0700

    Fix text file

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require 'byebug'
 
 class Board
   def self.empty_grid
@@ -22,11 +23,13 @@ class Board
   end
 
   def [](pos)
+    # debugger
     x, y = pos
     grid[x][y]
   end
 
   def []=(pos, value)
+    # debugger
     x, y = pos
     tile = grid[x][y]
     tile.value = value
